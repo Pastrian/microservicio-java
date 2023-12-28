6dd0bb3f4889e0d115cf37a7d5fdcd06e319a0d2
Pastrian <139199245+Pastrian@users.noreply.github.com>
2023-12-28 01:11:46 -0300

    Update HttpRequestTest.java

--- src/test/java/com/example/testingweb/HttpRequestTest.java
+++ src/test/java/com/example/testingweb/HttpRequestTest.java
@@ -22,6 +22,6 @@ public class HttpRequestTest {
 	@Test
 	public void greetingShouldReturnDefaultMessage() throws Exception {
 		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-				String.class)).contains("Feliz navidad te desea Felipe Pastrian");
+				String.class)).contains("Hola, soy Felipe Pastrian");
 	}
 }
